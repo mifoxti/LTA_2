8fb824ad064321abcba25ac2e810148408fe704f
Timofey Lyakhov <52703409+mifoxti@users.noreply.github.com>
2025-12-03 07:24:09 +0300

    Add comments to functions

--- calculator.py
+++ calculator.py
@@ -2,10 +2,20 @@ import math
 
 
 def get_area(a, b):
+   '''Функция для рассчета площади фигуры.
+   Args: int a, int b
+
+   returns: НЕПРАВИЛЬНЫЙ результат вычисления площади фигуры
+   '''
    return a * b
 
 
 def get_hypotenuse(a, b):
+   '''Функция для рассчета гипотенузы.
+   Args: int a, int b
+
+   returns: гипотенузу
+   '''
    return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
 
 
